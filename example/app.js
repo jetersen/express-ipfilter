14d90bdbd51ee037a9a495d529c512efea77fc5c
Joseph Petersen <josephp90@gmail.com>
2019-04-21 10:57:57 +0200

    update example

--- example/app.js
+++ example/app.js
@@ -1,73 +1,73 @@
-'use strict';
+'use strict'
 
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var favicon = require('serve-favicon');
+const express = require('express')
+const path = require('path')
+const logger = require('morgan')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const favicon = require('serve-favicon')
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-var ipfilter = require('express-ipfilter').IpFilter;
-var IpDeniedError = require('express-ipfilter').IpDeniedError;
+const routes = require('./routes/index')
+const users = require('./routes/users')
+const ipfilter = require('express-ipfilter').IpFilter
+const IpDeniedError = require('express-ipfilter').IpDeniedError
 
-var app = express();
+const app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-var ips = ['::ffff:127.0.0.1'];
-app.use(ipfilter(ips, {mode: 'allow'}));
+const ips = ['::ffff:127.0.0.1']
+app.use(ipfilter(ips, { mode: 'allow' }))
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', routes)
+app.use('/users', users)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+app.use((req, res, next) => {
+  const err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
 
 // error handlers
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, _next) {
-    console.log('Error handler', err);
-    if(err instanceof IpDeniedError){
-      res.status(401);
-    }else{
-      res.status(err.status || 500);
+  app.use((err, req, res) => {
+    console.log('Error handler', err)
+    if (err instanceof IpDeniedError) {
+      res.status(401)
+    } else {
+      res.status(err.status || 500)
     }
 
     res.render('error', {
       message: 'You shall not pass',
       error: err
-    });
-  });
+    })
+  })
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, _next) {
-  console.log('Error handler', err);
-  res.status(err.status || 500);
+app.use((err, req, res) => {
+  console.log('Error handler', err)
+  res.status(err.status || 500)
   res.render('error', {
     message: err.message,
     error: {}
-  });
-});
+  })
+})
 
-module.exports = app;
+module.exports = app
