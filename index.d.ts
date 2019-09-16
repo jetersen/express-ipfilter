e46284544695c6a26238b2aee1fbf9518374a435
Ravi S. Rāmphal <rramphal@gmail.com>
2019-09-16 00:30:32 -0400

    add Typescript types (#13)

---
+++ index.d.ts
@@ -0,0 +1,34 @@
+import express = require('express');
+
+export type Ip              = string;
+export type CidrRange       = string;
+export type Route           = string;
+export type StartEndIpRange = Ip[];
+export type IpRange         = CidrRange | StartEndIpRange;
+export type IpList          = Array<Ip | IpRange>;
+
+export interface IpCallback {
+  (): IpList;
+}
+
+export interface IpFilterOptions {
+  detectIp?: (request: express.Request) => Ip;
+  excluding?: Route[];
+  log?: boolean;
+  logLevel?: 'all' | 'deny' | 'allow';
+  mode?: 'deny' | 'allow';
+  // `@types/proxy-addr` does not export the `trust` parameter type
+  trustProxy?: any;
+}
+
+export function IpFilter(
+  ips: IpList | IpCallback,
+  opts?: IpFilterOptions,
+): express.RequestHandler;
+
+export class IpDeniedError extends Error {
+  constructor(
+    message: string,
+    extra: any,
+  );
+}
